fe0c51392bfdcbb59b1b2a0643c552b9744507c9
Krzysztof Czuryło <krzysztof.czurylo@gmail.com>
2017-10-31 18:44:46 +0100

    Implemented connection to SQLite databes

--- vocabularies.py
+++ vocabularies.py
@@ -1,1 +1,11 @@
 import sqlite3
+
+conn = sqlite3.connect('D:/reddit.db')
+cursor = conn.cursor()
+
+def get_comment_bodies():
+    cursor.execute('SELECT * FROM subreddits LIMIT 10')
+    for row in cursor:
+        print(row)
+
+get_comment_bodies()
