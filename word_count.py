99aae4c3c0f75bfbb57c7b75131542e113e2406f
Krzysztof Czuryło <krzysztof.czurylo@gmail.com>
2017-11-09 12:23:58 +0100

    Basic MR Job defined

--- word_count.py
+++ word_count.py
@@ -5,21 +5,13 @@ import time
 
 class MRWordCount(MRJob):
 
-    def configure_args(self):
-        super(MRWordCount, self).configure_args()
-        self.add_file_arg('--database')
-
-    def mapper_init(self):
-        self.sqlite_conn = sqlite3.connect(self.options.database)
-        subreddits_iterator = self.sqlite_conn.cursor()
-
-
-    def mapper(self, key, value):
-        for word in get_words_from_string(value):
+    def mapper(self, key, comment):
+        for word in get_words_from_string(comment):
             yield (word.lower(), 1)
 
     def reducer(self, key, values):
-        yield (key, sum(values))
+        if (sum(values) == 1):
+            yield key, None
 
 if __name__ == '__main__':
     MRWordCount.run()
