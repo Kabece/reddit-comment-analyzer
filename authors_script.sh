6ba0ccb976ba170f79ee317433b175face43905d
Krzysztof Czuryło <krzysztof.czurylo@gmail.com>
2017-11-12 20:25:28 +0100

    Script to run the authors.py program on DTU's HPC

---
+++ authors_script.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#PBS -q hpc
+#PBS -l walltime=01:30:00
+#PBS -l nodes=1:ppn=4
+#PBS -N Authors
+#PBS -l mem=16gb
+#PBS -m abe
+
+if test X$PBS_ENVIRONMENT = XPBS_BATCH; then cd $PBS_O_WORKDIR; fi
+
+module load python3/3.4.1
+
+python3 /zhome/c4/9/125030/Courses/authors.py /zhome/c4/9/125030/Courses/reddit.db
